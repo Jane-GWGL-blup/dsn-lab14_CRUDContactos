e224993d04909eebba0845f5d9f5728b78714af0
Nicole Caceres <janenieggcs@gmail.com>
2022-11-17 08:26:49 -0500

    String conection en el .env

--- db.js
+++ db.js
@@ -8,6 +8,6 @@ const MONGO_DB = 'procuctsv1';
 
 const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
 */
-const  url  =  `mongodb+srv://admin:Root1990@cluster0.umfobrr.mongodb.net/?retryWrites=true&w=majority` ;
 
+const  url  =  process.env.BD;
 mongoose.connect(url);
