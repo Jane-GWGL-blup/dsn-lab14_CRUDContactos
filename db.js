f04c72579750944e82d80cc9df11be2c85fb7fd8
Nicole Caceres <janenieggcs@gmail.com>
2022-11-24 10:08:51 -0500

    Arreglos en la BD

--- db.js
+++ db.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
-/*const MONGO_USERNAME = 'mongonc';
+const MONGO_USERNAME = 'mongonc';
 const MONGO_PASSWORD = 'mongo';
 const MONGO_HOSTNAME = '127.0.0.1';
 const MONGO_PORT = '27017';
 const MONGO_DB = 'procuctsv1';
 
 const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
-*/
+
 
 //const  url  =  process.env.BD;
 mongoose.connect(url);
